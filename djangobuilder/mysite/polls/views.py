afee85f3131076496a1340ba2b1b4418f1929898
ilya0 <iosovets@gmail.com>
2017-03-22 12:41:59 -0700

    added urlconf

--- djangobuilder/mysite/polls/views.py
+++ djangobuilder/mysite/polls/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse ("Hello, world. Your at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# this is the index for views
