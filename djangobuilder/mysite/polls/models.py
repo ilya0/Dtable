de1f3a4b83618db523bab9dd28579a8634c19000
ilya0 <iosovets@gmail.com>
2017-03-29 12:00:58 -0700

    run server runs - works

--- djangobuilder/mysite/polls/models.py
+++ djangobuilder/mysite/polls/models.py
@@ -22,5 +22,5 @@ class Choice(models.Model):
         return self.choice_text
 
     def was_published_recently(self):
-    now = timezone.now()
-    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
