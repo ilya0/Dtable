d36aeeaa46c6352a98739bee708e32a3e96079ee
ilya0 <iosovets@gmail.com>
2017-03-29 15:40:02 -0700

    nothing works - notworking

--- djangobuilder/mysite/polls/urls.py
+++ djangobuilder/mysite/polls/urls.py
@@ -19,15 +19,4 @@ urlpatterns = [
 ]
 
 
-# added indexs app
-app_name = 'indexs'
-urlpatterns = [
-    # index view
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    # details view
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # results view
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # vote view
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-]
+
