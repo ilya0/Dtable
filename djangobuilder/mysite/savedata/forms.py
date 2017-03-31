35ebcdc6a56b06318e1d76cef5bcee970ca12cdb
ilya0 <iosovets@gmail.com>
2017-03-31 09:19:52 -0700

    /savedata/submitform crashes the system but the app works, need to figure out the route problem to form - partially working

---
+++ djangobuilder/mysite/savedata/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class NameForm(forms.Form):
+    name = forms.CharField(label='Your name', max_length=100)
+    location = forms.CharField(max_length=200)
+    age = forms.CharField(max_length=3)
+    pub_date = forms.DateTimeField('date published')
--- djangobuilder/mysite/savedata/urls.py
+++ djangobuilder/mysite/savedata/urls.py
@@ -9,4 +9,6 @@ app_name = 'savedata'
 urlpatterns = [
     # index view
     url(r'^$', views.IndexView.as_view(), name='savedataindex'),
+    url(r'submitform^$', views.get_name, name='get_name'),
+
 ]
