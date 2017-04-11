b2b38007e8789659741a152cdde60ea96c87ca9a
ilya0 <iosovets@gmail.com>
2017-04-11 15:16:52 -0700

    added methods for delete button, python back end doesnt work - partially working

--- djangobuilder/mysite/savedata/static/savedata/js/addons.js
+++ djangobuilder/mysite/savedata/static/savedata/js/addons.js
@@ -1,5 +1,12 @@
+$("button").click(function() {
+    alert(this.id); // or alert($(this).attr('id'));
+});
+
+var buttonidholder;
+
 // Submit post on submit
 $('#post-form').on('submit', function(event){
+  alert(this.id)
     event.preventDefault();
     console.log("form submitted!"); // sanity check
     create_post();
@@ -34,6 +41,41 @@ function create_post() {
     });
 };
 
+
+//delete button methodology
+
+//on submit click prevent default and run create_post
+$('.deletebutton').click(function(event){
+    event.preventDefault();
+    console.log("form submitted!"); // sanity check
+    buttonidholder = this.id;
+    alert("buttonidholder is "+buttonidholder);
+    deletetable();
+});
+
+
+function deletetable() {
+      console.log("deletetable route hit"); // sanity check
+    $.ajax({
+        url : "deletetable/", // the endpoint
+        type : "POST", // http method
+        data : { the_post : $('#post-text').val() }, // data sent with the post request
+
+        // handle a successful response
+        success : function(json) {
+            $('#post-text').val(''); // remove the value from the input
+            console.log(json); // log the returned json to the console
+            console.log("success"); // another sanity check
+        },
+
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+};
 
 
 
