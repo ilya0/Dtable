f605ec359c42564994acdda786ed5968f83513d9
ilya0 <iosovets@gmail.com>
2017-04-11 11:35:17 -0700

    added js script to get value from text box, now creating ajax post route -workign

--- djangobuilder/mysite/savedata/static/savedata/js/inspinia.js
+++ djangobuilder/mysite/savedata/static/savedata/js/inspinia.js
@@ -9,6 +9,14 @@
 $(document).ready(function () {
 
 
+
+
+
+
+
+
+
+
     // Add body-small class if window less than 768px
     if ($(this).width() < 769) {
         $('body').addClass('body-small')
