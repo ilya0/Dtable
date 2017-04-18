dc5e64c8902b6b95697d56ecad0bafbd654bee0a
ilya o <iosovets@gmail.com>
2017-04-18 16:06:56 -0700

    working on a test page for new object oriented creation

---
+++ djangobuilder/mysite/savedata/test.py
@@ -0,0 +1,30 @@
+# this is a new class test
+
+
+# atributes are variables associated with an instance of class
+# a class is a type of thing
+# an instance is actual thing
+
+
+#
+# DTable - objects
+# DTColumn - objects
+# DTColumnType
+#     DTColumnTypeText, etc...
+# DTSchemaStore
+#     DTSchemaStoreJSON
+#     DTSchemaStoreSQL
+# DTDataEngine
+#     DTDataEngineJSON
+#     DTDataEngineSQL
+
+
+
+
+
+class Dtable(id,name,internalname)
+    def addcolumns
+
+    def deletecolumns
+
+
