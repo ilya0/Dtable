0cfb02d1c164f25cf46178ee43017fc272fcc2aa
ilya o <iosovets@gmail.com>
2017-04-21 13:30:38 -0700

    dtableapp works, dtschema storeapp does not work, its been disconnected from dtableapp -partially working

--- djangobuilder/mysite/mysite/settings.py
+++ djangobuilder/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'V2oop.apps.SavedataConfig',
     'savedata.apps.SavedataConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
